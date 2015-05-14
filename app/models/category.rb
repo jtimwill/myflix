a10890870fc16308b5585f03e94782e0f9c4b94c
jtimwill <jtimwill@gmail.com>
2015-05-13 21:00:52 -0400

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,8 @@
 class Category < ActiveRecord::Base
-<<<<<<< HEAD
-  has_many :videos
-=======
   has_many :videos, -> {order("created_at DESC")}
 
   def recent_videos
     videos.first(6)
   end
->>>>>>> mod1
+  
 end
