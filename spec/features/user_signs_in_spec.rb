51db888268e6fd8d12cc1599480235d6a20dbc00
Tim Williams <jtimwill@gmail.com>
2015-05-30 14:54:50 -0400

    Assignment: A more complicated feature spec

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -3,10 +3,7 @@ require 'spec_helper'
 feature 'User signs in' do 
   scenario "with existing username" do 
     alice = Fabricate(:user)
-    visit sign_in_path
-    fill_in "Email Address", with: alice.email
-    fill_in "Password", with: alice.password
-    click_button "Sign in"
-    expect(page).to have_content("You are signed in")
+    sign_in(alice)
+    expect(page).to have_content(alice.full_name)
   end
 end
