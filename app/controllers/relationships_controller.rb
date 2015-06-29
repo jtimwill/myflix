4cc7d9d8708b3b6844b006ee3ca3675ec10783c8
Tim Williams <jtimwill@gmail.com>
2015-06-28 23:08:11 -0400

    finished the follow a person assignment

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,6 @@
 class RelationshipsController < ApplicationController
   before_action :require_user
-  
+
   def index
     @relationships = current_user.following_relationships
   end
@@ -8,6 +8,12 @@ class RelationshipsController < ApplicationController
   def destroy
     relationship = Relationship.find(params[:id])
     relationship.destroy if relationship.follower == current_user
+    redirect_to people_path
+  end
+
+  def create
+    leader = User.find(params[:leader_id])
+    Relationship.create(leader_id: params[:leader_id], follower: current_user) if current_user.can_follow?(leader)
     redirect_to people_path
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,5 +22,20 @@ describe User do
       video = Fabricate(:video)
       expect(user.queued_video?(video)).to be_falsey
     end
+
+  describe "#follows?"
+    it "returns true if the user has a following relationship with another user" do 
+      alice = Fabricate(:user)
+      bob = Fabricate(:user)
+      Fabricate(:relationship, leader: bob, follower: alice)
+      expect(alice.follows?(bob)).to be_truthy
+    end
+
+    it "returns false if the user does not have a following relationship with another user" do 
+      alice = Fabricate(:user)
+      bob = Fabricate(:user)
+      Fabricate(:relationship, leader: alice, follower: bob)
+      expect(alice.follows?(bob)).to be_falsey
+    end 
   end
 end
