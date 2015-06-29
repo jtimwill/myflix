c27c49ab367197d55db68e1e52e1dc4999396473
Tim Williams <jtimwill@gmail.com>
2015-06-29 18:08:27 -0400

    added missing comma

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,12 +20,12 @@ Myflix::Application.configure do
 
   # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.default_url_options = {host: 'https://infinite-waters-5515.herokuapp.com/sign_in'}
-  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com'
+    domain:               'gmail.com',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
