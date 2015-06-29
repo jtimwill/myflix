2c2f114c67f2d54aa41720775c6577d3bfe62d60
Tim Williams <jtimwill@gmail.com>
2015-06-29 16:54:14 -0400

    added host in email production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,11 +19,11 @@ Myflix::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.delivery_method = :smtp
-
+  config.action_mailer.default_url_options = {host: 'https://infinite-waters-5515.herokuapp.com/sign_in'}
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
+    domain:               'heroku.com'
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
