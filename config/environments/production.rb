7aa6ed680da8d77a27de8905abc51b87b5123194
Tim Williams <jtimwill@gmail.com>
2015-06-29 17:55:36 -0400

    another attempt to get emails to work in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,13 +18,14 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = {host: 'https://infinite-waters-5515.herokuapp.com/sign_in'}
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_url_options = {host: 'https://infinite-waters-5515.herokuapp.com/sign_in'}
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'heroku.com'
+    domain:               'gmail.com'
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
