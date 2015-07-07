e73722fd5c97173d3bfdbf9a543be5a2ac12c500
Tim Williams <jtimwill@gmail.com>
2015-07-07 01:52:16 -0400

    updated config/unicorn.rb file

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,6 @@
-worker_processes 3
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
 
 before_fork do |server, worker|
    @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
