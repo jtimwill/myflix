126d26f66e672dd7b74cfa5ffec4eae694e101d1
Tim Williams <jtimwill@gmail.com>
2015-07-07 21:55:27 -0400

    updated raven config file

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,5 +1,8 @@
+if Rails.env.staging? || Rails.env.production?
 require 'raven'
 
-Raven.configure do |config|
-  config.dsn = 'SENTRY_DSN'
+  Raven.configure do |config|
+    config.dsn = 'SENTRY_DSN'
+  end
 end
+
