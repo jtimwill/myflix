00c50a194beed0bbcd4d94423e5703599c501c2e
Tim Williams <jtimwill@gmail.com>
2015-07-12 17:54:57 -0400

    revised aws credentials

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,9 +2,9 @@ CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
     config.storage = :aws
     config.aws_credentials = {
-      provider:              ENV['PROVIDER'],
       aws_access_key_id:     ENV['AWS_ACCESS_KEY'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region:                ENV['AWS_REGION']
     }
     config.aws_bucket  = ENV['S3_BUCKET_NAME']
   else
