d37c31cf7c397e422a5e89941f612c6d1c1d736f
Tim Williams <jtimwill@gmail.com>
2015-07-12 17:56:33 -0400

    added region to carrierwave config file

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,11 +2,11 @@ CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
     config.storage = :aws
     config.aws_credentials = {
-      aws_access_key_id:     ENV['AWS_ACCESS_KEY'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region:                ENV['AWS_REGION']
+      aws_access_key_id:     ENV.fetch('AWS_ACCESS_KEY'),
+      aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      region:                ENV.fetch('AWS_REGION')
     }
-    config.aws_bucket  = ENV['S3_BUCKET_NAME']
+    config.aws_bucket  = ENV.fetch('S3_BUCKET_NAME')
   else
     config.storage = :file
     config.enable_processing = Rails.env.development?
