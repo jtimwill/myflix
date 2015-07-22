7ad8e143261663e636bce95fe0913b987ed6e25c
jtimwill <jtimwill@gmail.com>
2015-07-22 11:18:38 -0400

    Updated hash syntax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,9 +24,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.valid?
       charge = StripeWrapper::Charge.create(
-        :amount => 999,
-        :source => params[:stripeToken],
-        :description => "Sign up charge for #{@user.email}"
+        amount: 999,
+        source: params[:stripeToken],
+        description: "Sign up charge for #{@user.email}"
       )
       if charge.successful?
         @user.save
