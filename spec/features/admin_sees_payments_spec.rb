b015617d7060bb2d33e88109996329926804a010
jtimwill <jtimwill@gmail.com>
2015-07-28 07:01:05 -0400

    updated last spec

--- spec/features/admin_sees_payments_spec.rb
+++ spec/features/admin_sees_payments_spec.rb
@@ -17,8 +17,8 @@ feature "Admin sees payments" do
   scenario "user cannot see payments" do
     sign_in(Fabricate(:user))
     visit admin_payments_path
-    expect(page).not_to have_content("$9.99")
-    expect(page).not_to have_content("Alice Doe")
+    expect(page).to have_no_content("$9.99")
+    expect(page).to have_no_content("Alice Doe")
     expect(page).to have_content("You are not authorized to do that.")
   end
 end
